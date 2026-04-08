f5e39fc47483a665a3d4caaebd2cdde01e62d931
krishnavgogoi <krishnavgogoi08@gmail.com>
2026-04-08 12:21:50 +0530

    cursor

--- indx.js
+++ indx.js
@@ -263,6 +263,22 @@ gsap.from("#page9  .brandblock p ", {
 
 
 
+
+
+gsap.set(".flair", {xPercent: -50, yPercent: -50});
+
+let xTo = gsap.quickTo(".flair", "x", {duration: 0.6, ease: "power3"}),
+    yTo = gsap.quickTo(".flair", "y", {duration: 0.6, ease: "power3"});
+
+window.addEventListener("mousemove", e => {
+  xTo(e.clientX);
+  yTo(e.clientY);
+});
+
+
+
+
+
 // =============================================
 //  TOP HEADER — hamburger menu
 // =============================================
